fc9b6b5bc900732913141f83ba10340268bd1531
Felipi <felipi@teste.com>
2026-09-11 15:14:46 -0300

    Adiciona teste para inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<Post />);
+
+    const campoComentario = screen.getByTestId("campo-comentario");
+    const botaoComentar = screen.getByTestId("botao-comentar");
+
+    fireEvent.change(campoComentario, {
+      target: { value: "Primeiro comentário" },
     });
+
+    fireEvent.click(botaoComentar);
+
+    fireEvent.change(campoComentario, {
+      target: { value: "Segundo comentário" },
+    });
+
+    fireEvent.click(botaoComentar);
+    expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
+    expect(screen.getByText("Segundo comentário")).toBeInTheDocument();
+  });
 });
